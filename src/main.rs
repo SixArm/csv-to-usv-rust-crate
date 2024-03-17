823fcbb0f15b42ccb52c86431df37f29be124325
Joel Parker Henderson <joel@joelparkerhenderson.com>
2024-03-17 19:55:23 +0000

    Revise typo

--- src/main.rs
+++ src/main.rs
@@ -109,7 +109,7 @@
 //!
 //! * Package: csv-to-usv-rust-crate
 //! * Version: 1.2.0
-//! * Created: 2024-03-09-13:33:20Z
+//! * Created: 2024-03-09T13:33:20Z
 //! * Updated: 2024-03-14T13:38:46Z
 //! * License: MIT or Apache-2.0 or GPL-2.0 or GPL-3.0 or contact us for more
 //! * Contact: Joel Parker Henderson (joel@sixarm.com)
